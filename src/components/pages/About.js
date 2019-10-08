5d3adf17783e7a4862bbbb28f1566871df2c0621
HopeEternal <risalee316@gmail.com>
2019-10-08 16:48:34 -0600

    Router enabled and About page added.

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a simple and light-wiehgt TodoList app v1.0.0. It uses Axios, JSON Placeholder and React state management.</p> 
+        </React.Fragment>
+    )
+}
